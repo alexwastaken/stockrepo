1b9c607b53d6112dde47fbaf99616d154745762e
alex parsons <somedevbwahh@gmail.com>
2016-05-28 14:11:14 -0700

    added a basic login html css

---
+++ public/css/loginsystem.js
@@ -0,0 +1,3 @@
+$(document).ready(function () {
+    $('div#loginBody').fadeIn(500).removeClass('hidden');
+});
---
+++ public/js/loginsystem.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+	$( ".btn" ).click(function() {
+		$('div#loginBody').fadeIn(500)
+		$('div#backgroundColorLogin').fadeIn(500)
+	});
+});
